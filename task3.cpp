8ead92a1a64b3bdf6aa45b6f9a70735dbb61502e
makskrupski <79255479+makskrupski@users.noreply.github.com>
2021-03-11 13:22:41 +0200

    Add files via upload

--- task3.cpp
+++ task3.cpp
@@ -1,1 +1,21 @@
 #include <iostream>
+using namespace std;
+
+int element(int i)
+{
+	if (i == 0)
+		return 1;
+	else if (i == 1)
+		return 2;
+	return 2 * element(i - 1) + 4 * element(i - 2) + 1;
+
+}
+
+int sum(int N)
+{
+	int res = 0;
+	for (int i = 0; i < N; i++) {
+		res += element(i);
+	}
+	return res;
+}
